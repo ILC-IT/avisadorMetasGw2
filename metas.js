852ae5e98b675f5dc171701b7321cca7d4f216d5
ILC-IT <71129685+ILC-IT@users.noreply.github.com>
2026-07-17 12:18:05 +0200

    +filtro expansiones

--- metas.js
+++ metas.js
@@ -18,174 +18,232 @@ function ordenarHoras(horas) {
 const eventosOriginales = [
   {
     nombre: "Verdant Brink",
+    expansiones: ["HoT"],
     horarios: ["00:09", "02:09", "04:09", "06:09", "08:09", "10:09", "12:09", "14:09" , "16:09", "18:09", "20:09", "22:09"],
     puntoRuta: "[&BAgIAAA=]"
   },
   {
     nombre: "Tangled Depths",
+    expansiones: ["HoT"],
     horarios: ["00:30", "02:30", "04:30", "06:30", "08:30", "10:30", "12:30", "14:30" , "16:30", "18:30", "20:30", "22:30"],
     puntoRuta: "[&BPUHAAA=]"
   },
     {
     nombre: "Auric Basin",
+    expansiones: ["HoT"],
     horarios: ["01:00", "03:00", "05:00", "07:00", "09:00", "11:00", "13:00", "15:00" , "17:00", "19:00", "21:00", "23:00"],
     puntoRuta: "[&BGwIAAA=]"
   },
   {
     nombre: "Dragon's Stand",
+    expansiones: ["HoT"],
     horarios: ["01:30", "03:30", "05:30", "07:30", "09:30", "11:30", "13:30", "15:30" , "17:30", "19:30", "21:30", "23:30"],
     puntoRuta: "[&BBAIAAA=]"
   },
   {
     nombre: "Pinata",
+    expansiones: ["PoF"],
     horarios: ["00:05", "02:05", "04:05", "06:05", "08:05", "10:05", "12:05", "14:05", "16:05", "18:05", "20:05", "22:05"],
     puntoRuta: "[&BLsKAAA=]"
   },
   {
     nombre: "Desert Highlands",
+    expansiones: ["PoF"],
     horarios: ["01:00", "03:00", "05:00", "07:00", "09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00"],
     puntoRuta: "[&BGsKAAA=]"
   },
     {
     nombre: "Elon Riverlands",
+    expansiones: ["PoF"],
     horarios: ["01:30", "03:30", "05:30", "07:30", "09:30", "11:30", "13:30", "15:30", "17:30", "19:30", "21:30", "23:30"],
     puntoRuta: "[&BFMKAAA=]"
   },
   {
     nombre: "The Desolation Maws",
+    expansiones: ["PoF"],
     horarios: ["01:00", "03:00", "05:00", "07:00", "09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00"],
     puntoRuta: "[&BKMKAAA=]"
   },
   {
     nombre: "The Desolation Junundu",
+    expansiones: ["PoF"],
     horarios: ["00:30", "01:30", "02:30", "03:30", "04:30", "05:30", "06:30", "07:30", "08:30", "09:30", "10:30", "11:30", "12:30", "13:30", "14:30", "15:30", "16:30", "17:30", "18:30", "19:30", "20:30", "21:30", "22:30", "23:30"],
     puntoRuta: "[&BMEKAAA=]"
   },
     {
     nombre: "Domain of Vabbi Dogs",
+    expansiones: ["PoF"],
     horarios: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"],
     puntoRuta: "[&BO0KAAA=]"
   },
   {
     nombre: "Domain of Vabbi Serpent's Ire",
+    expansiones: ["PoF"],
     horarios: ["00:30", "02:30", "04:30", "06:30", "08:30", "10:30", "12:30", "14:30", "16:30", "18:30", "20:30", "22:30"],
     puntoRuta: "[&BHQKAAA=]"
   },
   {
     nombre: "Istan Palawadan",
+    expansiones: ["LW4"],
     horarios: ["01:45", "03:45", "05:45", "07:45", "09:45", "11:45", "13:45", "15:45", "17:45", "19:45", "21:45", "23:45"],
     puntoRuta: "[&BAkLAAA=]"
   },
   {
     nombre: "Jahai DBS",
+    expansiones: ["LW4"],
     horarios: ["01:15", "03:15", "05:15", "07:15", "09:15", "11:15", "13:15", "15:15", "17:15", "19:15", "21:15", "23:15"],
     puntoRuta: "[&BJMLAAA=]"
   },
   {
     nombre: "Thunderhead Peaks North",
+    expansiones: ["LW4"],
     horarios: ["01:45", "03:45", "05:45", "07:45", "09:45", "11:45", "13:45", "15:45", "17:45", "19:45", "21:45", "23:45"],
     puntoRuta: "[&BLsLAAA=]"
   },
   {
     nombre: "Thunderhead Peaks South",
+    expansiones: ["LW4"],
     horarios: ["00:45", "02:45", "04:45", "06:45", "08:45", "10:45", "12:45", "14:45", "16:45", "18:45", "20:45", "22:45"],
     puntoRuta: "[&BKYLAAA=]"
   },
   {
     nombre: "Grothmar Valley Ooze",
+    expansiones: ["IBS"],
     horarios: ["01:05", "03:05", "05:05", "07:05", "09:05", "11:05", "13:05", "15:05", "17:05", "19:05", "21:05", "23:05"],
     puntoRuta: "[&BPgLAAA=]"
   },
   {
     nombre: "Grothmar Valley Metal Concert",
+    expansiones: ["IBS"],
     horarios: ["01:40", "03:40", "05:40", "07:40", "09:40", "11:40", "13:40", "15:40", "17:40", "19:40", "21:40", "23:40"],
     puntoRuta: "[&BPgLAAA=]"
   },
   {
     nombre: "Grothmar Valley Effigy",
+    expansiones: ["IBS"],
     horarios: ["00:10", "02:10", "04:10", "06:10", "08:10", "10:10", "12:10", "14:10", "16:10", "18:10", "20:10", "22:10"],
     puntoRuta: "[&BA4MAAA=]"
   },
   {
     nombre: "Grothmar Valley Doomlore",
+    expansiones: ["IBS"],
     horarios: ["00:38", "02:38", "04:38", "06:38", "08:38", "10:38", "12:38", "14:38", "16:38", "18:38", "20:38", "22:38"],
     puntoRuta: "[&BA4MAAA=]"
   },
   {
     nombre: "Bjora Joras's Keep",
+    expansiones: ["IBS"],
     horarios: ["01:45", "03:45", "05:45", "07:45", "09:45", "11:45", "13:45", "15:45", "17:45", "19:45", "21:45", "23:45"],
     puntoRuta: "[&BCcMAAA=]"
   },
   {
     nombre: "Seitung Province",
+    expansiones: ["EoD"],
     horarios: ["01:30", "03:30", "05:30", "07:30", "09:30", "11:30", "13:30", "15:30", "17:30", "19:30", "21:30", "23:30"],
     puntoRuta: "[&BGUNAAA=]"
   },
   {
     nombre: "New Kaineng City",
+    expansiones: ["EoD"],
     horarios: ["00:00", "02:00", "04:00", "06:00", "08:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"],
     puntoRuta: "[&BBkNAAA=]"
   },
   {
     nombre: "The Echovald Wilds Gang War",
+    expansiones: ["EoD"],
     horarios: ["00:30", "02:30", "04:30", "06:30", "08:30", "10:30", "12:30", "14:30", "16:30", "18:30", "20:30", "22:30"],
     puntoRuta: "[&BMwMAAA=]"
   },
   {
     nombre: "The Echovald Wilds Turtles",
+    expansiones: ["EoD"],
     horarios: ["01:40", "03:40", "05:40", "07:40", "09:40", "11:40", "13:40", "15:40", "17:40", "19:40", "21:40", "23:40"],
     puntoRuta: "[&BPkMAAA=]"
   },
   {
     nombre: "Dragon's End",
+    expansiones: ["EoD"],
     horarios: ["01:00", "03:00", "05:00", "07:00", "09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00"],
     puntoRuta: "[&BKIMAAA=]"
   },
   {
     nombre: "Skywatch Archipelago",
+    expansiones: ["Soto"],
     horarios: ["01:00", "03:00", "05:00", "07:00", "09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00"],
     puntoRuta: "[&BL4NAAA=]"
   },
   {
     nombre: "Amnytas",
+    expansiones: ["Soto"],
     horarios: ["00:00", "02:00", "04:00", "06:00", "08:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"],
     puntoRuta: "[&BDQOAAA=]"
   },
   {
     nombre: "Janthir Syntri",
+    expansiones: ["JW"],
     horarios: ["00:30", "02:30", "04:30", "06:30", "08:30", "10:30", "12:30", "14:30", "16:30", "18:30", "20:30", "22:30"],
     puntoRuta: "[&BCoPAAA=]"
   },
   {
     nombre: "Bava Nisos",
+    expansiones: ["JW"],
     horarios: ["01:20", "03:20", "05:20", "07:20", "09:20", "11:20", "13:20", "15:20", "17:20", "19:20", "21:20", "23:20"],
     puntoRuta: "[&BGEPAAA=]"
   },
   {
     nombre: "Shipwreck Strand",
+    expansiones: ["VoE"],
     horarios: ["00:40", "02:40", "04:40", "06:40", "08:40", "10:40", "12:40", "14:40", "16:40", "18:40", "20:40", "22:40"],
     puntoRuta: "[&BJEPAAA=]"
   },
   {
     nombre: "Starlit Weald",
+    expansiones: ["VoE"],
     horarios: ["01:40", "03:40", "05:40", "07:40", "09:40", "11:40", "13:40", "15:40", "17:40", "19:40", "21:40", "23:40"],
     puntoRuta: "[&BJ4PAAA=]"
   },
   {
     nombre: "Eternity's Garden",
+    expansiones: ["VoE"],
     horarios: ["1:10", "3:10", "5:10", "7:10", "9:10", "11:10", "13:10", "15:10", "17:10", "19:10", "21:10", "23:10"],
     puntoRuta: "[&BPwPAAA=]"
   },
   {
     nombre: "Convergence Soto",
+    expansiones: ["Convergence"],
     horarios: ["00:30", "03:30", "06:30", "09:30", "12:30", "15:30", "18:30", "21:30"],
     puntoRuta: "[&BB8OAAA=]"
   },
   {
     nombre: "Convergence JW",
+    expansiones: ["Convergence"],
     horarios: ["02:00", "05:00", "08:00", "11:00", "14:00", "17:00", "20:00", "23:00"],
     puntoRuta: "[&BK4OAAA=]"
-  }
+  },
+  {
+    nombre: "Dragonstorm",
+    expansiones: ["Public"],
+    horarios: ["01:00", "03:00", "05:00", "07:00", "09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00"],
+    puntoRuta: "[&BAkMAAA=]"
+  },
+  {
+    nombre: "Tower of Nightmares",
+    expansiones: ["Public"],
+    horarios: ["01:30", "03:30", "05:30", "07:30", "09:30", "11:30", "13:30", "15:30", "17:30", "19:30", "21:30", "23:30"],
+    puntoRuta: "[&BAkMAAA=]"
+  },
+  {
+    nombre: "Twisted Marionette",
+    expansiones: ["Public"],
+    horarios: ["00:00", "02:00", "04:00", "06:00", "08:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"],
+    puntoRuta: "[&BAkMAAA=]"
+  },
+  {
+    nombre: "Battle For Lion's Arch",
+    expansiones: ["Public"],
+    horarios: ["00:30", "02:30", "04:30", "06:30", "08:30", "10:30", "12:30", "14:30", "16:30", "18:30", "20:30", "22:30"],
+    puntoRuta: "[&BAkMAAA=]"
+  },
 ];
 
 // Creacion de eventosOriginales con las horas ajustada SIN modificar la variable original
